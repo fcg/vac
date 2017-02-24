c4365af9c1e1a9f79f2e13953576a1bed2def2f0
lxdigg <daydaydayupupup@gmail.com>
2017-02-24 16:21:35 +0800

    mbeoi

---
+++ _posts/2016-12-30-MPNP-EOI-Draw-25.md
@@ -0,0 +1,28 @@
+---
+layout: post
+title:  "曼省技术移民 EOI 2016-12-30 总第 25 捞：境外 20 人，787 分；曼省内 181 人，702 分"
+date:   2016-12-30 23:56:00  +0800
+categories: MB
+---
+
+飞出国：加拿大时间 2016-12-30 曼省省提名技术移民 EOI 第 25 捞：境外 20 人，787 分，曼省境内 181 人，702 分。
+
+- 曼省境内 [SKILLED WORKERS IN MANITOBA]：
+  - Number of Letters of Advice to Apply issued: 181
+  - Ranking score of lowest-ranked candidate invited:702
+- 境外 [SKILLED WORKERS OVERSEAS]：
+  - Number of Letters of Advice to Apply issued: 20 
+  - Ranking score of lowest-ranked candidate invited: 787 分
+
+详细的请参考飞出国论坛：[曼省EOI邀请记录]。
+
+需要获得相关移民及出国签证申请帮助可以联系飞出国微信（fcgvisabbs）： <a href="http://flyabroad.me/contact" target="_blank">http://flyabroad.me</a>。
+
+> 以上内容由`飞出国香港`（<a href="http://flyabroad.hk/" target="_blank">flyabroad.hk</a>）整理完成，转载请保留并注明出处。
+
+------
+
+[曼省EOI邀请记录]: http://bbs.fcgvisa.com/t/eoi-mpnp-under-the-expression-of-interest-system-draws/3723
+[SKILLED WORKERS IN MANITOBA]: http://bbs.fcgvisa.com/t/swm-eligibility-mpnp-skilled-workers-in-manitoba/3684
+[SKILLED WORKERS OVERSEAS]: http://bbs.fcgvisa.com/t/swo-eligibility-mpnp-skilled-workers-overseas/3698
+
--- eegrab/mbeoi.rb
+++ eegrab/mbeoi.rb
@@ -1,6 +1,3 @@
-# http://www.immigratemanitoba.com/mpnp-for-skilled-workers/expression-of-interest/
-# http://www.immigratemanitoba.com/eoi-draw-archive/
-
 require 'Nokogiri'
 require 'open-uri'
 require 'haml'
@@ -11,37 +8,56 @@ require "yaml"
 require 'date'
 require 'openssl'
 
-TURL = 'http://www.immigratemanitoba.com/mpnp-for-skilled-workers/expression-of-interest/'
+TURL = 'http://www.immigratemanitoba.com/news-and-notices/'
 
 def get_doc(url)
 
 	ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:9.0.1) Gecko/20100101 Firefox/9.0.1'
 	charset = 'utf-8'
 	error = nil;
-  # html = open("vic-graduates.html")
 	html = open(url, 'User-Agent' => ua)
 	doc = Nokogiri::HTML::parse(html, nil, charset)
 end
 
-def post()
+def parse_eoi(url)
 
   db = SQLite3::Database.open "eedraws.db"
 
-db.results_as_hash = false
+puts 'parse new mbeoi and insert into db'
+
+  doc = get_doc(url)
+
+  puts h2s = doc.css(".entry-content>p>strong")[1].text
+
+  num,date = h2s.strip.split("#")[1].split("–")
+  
+  p num = num.strip
+  p updated = Date.parse(date.gsub("\u00A0","").strip).strftime("%Y-%m-%d")
+
+  strongs = doc.css(".entry-content>ul>li>strong")
+
+  p inmbnum = strongs[0].text.split(":").last.strip.gsub(" ","")
+  p inmbrank =   strongs[1].text.split(":").last.strip.gsub(" ","")
+  p overseasnum = strongs[2].text.split(":").last.strip.gsub(" ","").split(" ")[0]
+  p overseasrank =   strongs[3].text.split(":").last.strip.gsub(" ","")
+
+  db.execute("insert into mbeoi (num,inmbnum,inmbrank,overseasnum,overseasrank,updated) values (?,?,?,?,?,?)",[num,inmbnum,inmbrank,overseasnum,overseasrank,updated])
+ 
+puts '从数据库重新生成 csv'
+
+  db.results_as_hash = false
+
   rows = db.execute("select num,updated,inmbnum,inmbrank,overseasnum,overseasrank from mbeoi order by num asc")
 
-db.results_as_hash = true
+  db.results_as_hash = true
   sums = db.execute("select max(num) nums,sum(inmbnum)+sum(overseasnum) ioall, sum(inmbnum) inall, sum(overseasnum) outall, min(inmbrank) minin, max(inmbrank) maxin,min(overseasrank) minoverseas,max(overseasrank)  maxoverseas from mbeoi")
 
- puts "Results = #{sums[0].inspect}"
+  puts "Results = #{sums[0].inspect}"
 
-  mbeoidir = 'D:\FCGGH\vac\_data\mb'
+  mbeoidir = '../_data/mb'
+  postdir = '../_posts/'
   csvname = 'eoi2016.csv'
-  ymlname = 'sum2016.yml'
-
-  # d = YAML::load_file("#{mbeoidir}/#{ymlname}") #Load
-  # d['content'] = 2 #Modify
-  File.open("#{mbeoidir}/#{2016/7/30}", 'w+') {|f| f.write sums[0].to_yaml }
+  ymlname = 'sum2016.yml'    
 
   CSV.open("#{mbeoidir}/#{csvname}","w") do |csv|
 
@@ -56,105 +72,56 @@ db.results_as_hash = true
     csv << ["总:","飞出国",sum[2],sum[4],sum[3],sum[6]]
   end
 
-end
+puts '生成 post'
 
-def parse_new()
-  db = SQLite3::Database.open "eedraws.db"
+frontstr = <<-YAML
+---
+layout: post
+title:  "曼省技术移民 EOI #{updated} 总第 #{num} 捞：境外 #{overseasnum} 人，#{overseasrank} 分；曼省内 #{inmbnum} 人，#{inmbrank} 分"
+date:   #{updated} 23:56:00  +0800
+categories: MB
+---
+YAML
 
-  doc = get_doc(TURL)
-  h2s = doc.css("h2")
 
-  num,no = h2s.first.text.strip.split("#")[1].split("–")
+intrstr = <<-INTR
 
-  date = h2s[1].text.strip
+飞出国：加拿大时间 #{updated} 曼省省提名技术移民 EOI 第 #{num} 捞：境外 #{overseasnum} 人，#{overseasrank} 分，曼省境内 #{inmbnum} 人，#{inmbrank} 分。
 
-  num = num.strip
+- 曼省境内 [SKILLED WORKERS IN MANITOBA]：
+  - Number of Letters of Advice to Apply issued: #{inmbnum}
+  - Ranking score of lowest-ranked candidate invited:#{inmbrank}
+- 境外 [SKILLED WORKERS OVERSEAS]：
+  - Number of Letters of Advice to Apply issued: #{overseasnum} 
+  - Ranking score of lowest-ranked candidate invited: #{overseasrank} 分
 
-  updated = Date.parse(date.strip).strftime("%Y-%m-%d")
+详细的请参考飞出国论坛：[曼省EOI邀请记录]。
 
-  strongs = doc.css("p>strong")
+需要获得相关移民及出国签证申请帮助可以联系飞出国微信（fcgvisabbs）： <a href="http://flyabroad.me/contact" target="_blank">http://flyabroad.me</a>。
 
-  inmbnum = strongs[0].text.split(":").last.strip.gsub(" ","")
-  inmbrank =   strongs[1].text.split(":").last.strip.gsub(" ","")
-  overseasnum = strongs[2].text.split(":").last.strip.gsub(" ","").split(" ")[0]
-  overseasrank =   strongs[3].text.split(":").last.strip.gsub(" ","")
+> 以上内容由`飞出国香港`（<a href="http://flyabroad.hk/" target="_blank">flyabroad.hk</a>）整理完成，转载请保留并注明出处。
 
-  db.execute("insert into mbeoi (num,inmbnum,inmbrank,overseasnum,overseasrank,updated) values (?,?,?,?,?,?)",[num,inmbnum,inmbrank,overseasnum,overseasrank,updated])
+------
 
-end
+[曼省EOI邀请记录]: http://bbs.fcgvisa.com/t/eoi-mpnp-under-the-expression-of-interest-system-draws/3723
+[SKILLED WORKERS IN MANITOBA]: http://bbs.fcgvisa.com/t/swm-eligibility-mpnp-skilled-workers-in-manitoba/3684
+[SKILLED WORKERS OVERSEAS]: http://bbs.fcgvisa.com/t/swo-eligibility-mpnp-skilled-workers-overseas/3698
 
-parse_new()
-post()
+INTR
 
-def parse_archive()
+    mbpostfile = "#{updated}-MPNP-EOI-Draw-#{num}"
 
-	db = SQLite3::Database.open "eedraws.db"
+    File.open("#{postdir}#{mbpostfile}.md", 'w') do |file|
 
-  # doc = get_doc(ATURL)
+      content = frontstr + intrstr
 
-  html = open("mbeoi.htm")
-  charset = 'utf-8'
+      file.write content
 
-  doc = Nokogiri::HTML::parse(html, nil, charset)
+    end
 
-  h2s = doc.css("h2")
-  # h3s = doc.css("h3")
-  strongs = doc.css("p>strong")
-
-  puts strongs.size
-
-  h2s.each_with_index do |mh2,index|
-
-    i = index + 1
-
-# MPNP under the Expression of Interest System – Draw #1 – May 20, 2015
-    no,date = mh2.text.strip.split("#")[1].split("–")
-
-    no = no.strip
-
-    updated = Date.parse(date.strip).strftime("%Y-%m-%d")
-
-    # ionoff = i*2 Skilled Workers in Manitoba - Skilled Workers Overseas
-    # puts h3s[ionoff].text + " " + h3s[ionoff+1].text
-    # num, rank
-begin
-    istrong = i*5
-
-    inmbnum = strongs[istrong-5].text.split(":").last.strip.gsub(" ","")
-    inmbrank =   strongs[istrong-4].text.split(":").last.strip.gsub(" ","")
-    overseasnum = strongs[istrong-3].text.split(":").last.strip.gsub(" ","")
-    overseasrank =   strongs[istrong-2].text.split(":").last.strip.gsub(" ","")
-
-    db.execute("insert into mbeoi (num,inmbnum,inmbrank,overseasnum,overseasrank,updated) values (?,?,?,?,?,?)",[no,inmbnum,inmbrank,overseasnum,overseasrank,updated])
-  rescue Exception => ex
-    puts ex.message
-    puts strongs[istrong]
-  end
-
-  end
 
 end
 
-# parse_archive()
-
-def creatembeoi()
-
-	db = SQLite3::Database.open "eedraws.db"
-
-	rows = db.execute <<-SQL
-		create table mbeoi (
-			Id INTEGER PRIMARY KEY AUTOINCREMENT,
-			num INTEGER DEFAULT NULL,
-			inmbnum INTEGER DEFAULT NULL,
-			inmbrank INTEGER DEFAULT NULL,
-			overseasnum INTEGER DEFAULT NULL,
-			overseasrank INTEGER DEFAULT NULL,
-			updated TEXT DEFAULT NULL
-		);
-	SQL
-
-	db.close
-
-end
-
-# creatembeoi()
+parse_eoi("http://www.immigratemanitoba.com/2016/12/21/eoi-draw-no-24/")
+parse_eoi("http://www.immigratemanitoba.com/2016/12/30/eoi-draw-no-25/")
+parse_eoi("http://www.immigratemanitoba.com/2017/01/30/eoi-draw-no-26/")
