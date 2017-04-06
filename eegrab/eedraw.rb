b49dfe399c495e082fdbf78170cce1ebdecea001
lxdigg <daydaydayupupup@gmail.com>
2017-04-06 11:07:33 +0800

    ee

--- eegrab/eedraw.rb
+++ eegrab/eedraw.rb
@@ -119,6 +119,8 @@ YAML
 
 飞出国：加拿大时间 #{eedate}，CIC 发布 Express Entry 2017 年第 #{numinyear} 捞（总第 #{currentNum} 捞），#{eeinvitations}人，#{eerank}分。
 
+2016年11月19日，加拿大联邦技术移民快速通道类别更改评分标准，[降低job offer加分，增加加拿大学历加分](http://bbs.fcgvisa.com/t/significant-changes-to-comprehensive-ranking-system-crs-for-express-entry-immigration-system/19886)，2017年6月6日，EE CRS 进一步调整评分标准，增加[加拿大亲属分数及法语额外加分](http://www.flyabroadnews.com/express-entry-comprehensive-ranking-system-crs-2017-flyabroad/)。
+
 加拿大联邦技术移民 [Express Entry] 不要求申请人有雇主offer，也没有职业和专业限制，对加拿大受管制职业也不要求获得省政府的认证，只需要满足加拿大联邦技术移民 [FSW]，加拿大经验类移民 [CEC] 或 加拿大技工类移民 [FST] 任何一种加拿大联邦技术移民签证类别要求就可以创建 Express Entry profile 到池子排队（EE pool），然后加拿大联邦移民部会不定期根据分数高低发出 EE 邀请，获得邀请后90天内提交移民签证申请，提交移民签证申请后有半年左右时间就可以获得签证了。
 
 2016年11月19日，加拿大 Express Entry CRS 评分标准调整，加拿大雇主 offer 大部分只能加 50 分，600 分以上申请人明显减少，同时随着加拿大联邦 EE 邀请人数增加，EE 邀请分数已经大大低于 450 分，当前最低是 #{minrank} 分就可以获得邀请，这给学历高，语言好，有加拿大学历或加拿大工作经验的申请人提供了方便快捷的申请方式，随着 #{minrank} 分上申请人被联邦捞取，加拿大安大略省省提名项目 400 分类别申请人里大部分获得邀请的应该是 400 - #{minrank} 之间申请人，对安省省提名 [HCP] 类别也是利好消息。
