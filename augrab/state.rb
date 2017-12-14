0e2de37146de0884acd875f931385b1b943e3926
lxdigg <daydaydayupupup@gmail.com>
2017-12-14 20:20:12 +0800

    state-0930

--- augrab/state.rb
+++ augrab/state.rb
@@ -13,8 +13,8 @@ CEILINGTRS = "//*[@id='tab-content-3']/table/tbody/tr".freeze
 TURL = "http://www.border.gov.au/Trav/Work/Skil".freeze
 
 ### 注意，每次修改这里为当前发布数据月份的最后一天
-F1718 = '2017-10-31'.freeze
-MONTH = '2017-10'.freeze # 每次修改这里
+F1718 = '2017-11-28'.freeze # 每月都有28
+MONTH = '2017-11'.freeze # 每次修改这里
 
 T190CSV = '190-1718'.freeze
 ZDBTOTAL = 'zdb-total-1718'.freeze
