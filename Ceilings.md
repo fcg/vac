152f71f3653195037af1d7f2cc9fef281eeadc30
flyeven <00hou.me@gmail.com>
2018-08-05 22:52:27 +0800

    ceilings1

--- Ceilings.md
+++ Ceilings.md
@@ -5,9 +5,9 @@ title: 澳洲 2017-2018 SOL 配额
 
 ## 澳大利亚技术移民2017-2018年度 SOL 职业(189+489亲属)配额完成情况汇总
 
->Occupation ceilings for the 2017-18 programme year
+>Occupation ceilings for the 2018-19 programme year
 
-飞出国：下表基于为2017-2018年度澳大利亚技术移民SOL配额使用情况汇总表。
+飞出国：下表基于为2018-2019年度澳大利亚技术移民SOL配额使用情况汇总表。
 
 飞出国下表排序是按照剩余职业配额由少到多排序的。
 
@@ -19,7 +19,7 @@ title: 澳洲 2017-2018 SOL 配额
 <th>已邀请</th>
 <th>所剩配额</th>
 </tr>
-{% for c in site.data.sol.ceilling-17-18 %}
+{% for c in site.data.sol.ceilling-18-19 %}
 <tr>
 <td> <a href="http://bbs.fcgvisa.com/t/topic/{{ c.bbsid }}" target="_blank">{{ c.anzsco4 }}</a> </td>
 <td> {{ c.namecn }} </td>
@@ -32,6 +32,8 @@ title: 澳洲 2017-2018 SOL 配额
 
 更多说明请参考<a href="http://bbs.fcgvisa.com/t/17031/" target="blank">飞出国论坛</a> 。
 
-需要获得相关移民及出国签证申请帮助可以联系飞出国微信（flyabroad）： <a href="http://flyabroad.me/contact" target="_blank">http://flyabroad.me/contact/</a>。
+[荷兰库拉索移民](http://www.flyabroad.hk/curacao)适合技术移民无望或技术移民遥遥无期的高知中产阶层人群。一套提供持续较高收益的国际房产（酒店公寓），一个说走就走的国际身份（无移民监），一个中产阶层与欧洲强国护照最接近的移民项目（荷兰护照）。
+
+需要获得相关移民及出国签证申请帮助可以联系飞出国： <a href="http://flyabroad.me/contact" target="_blank">http://flyabroad.me/contact/</a>。
 
 > 以上内容由`飞出国香港`（<a href="http://flyabroad.hk/" target="_blank">flyabroad.hk</a>）整理完成，转载请保留并注明出处。
--- augrab/ceiling.rb
+++ augrab/ceiling.rb
@@ -11,10 +11,10 @@ require 'reverse_markdown'
 CEILINGS   = ".//*[@id='tab-content-3']/table"
 CEILINGTRS = ".//*[@id='tab-content-3']/table/tbody/tr"
 
-TURL = "https://www.homeaffairs.gov.au/trav/work/skil"
+TURL = "https://www.homeaffairs.gov.au/Busi/Empl/skillselect"
 
-CURRENTFN = "2018-06-20"  # 每次有新更新先修改这里
-F1718 = "ceilling-17-18"
+CURRENTFN = "2018-07-11"  # 每次有新更新先修改这里
+F1819 = "ceilling-18-19"
 DATADIR = "../_data/sol/"
 POSTDIR = "../_posts/"
 
@@ -43,10 +43,13 @@ def initdb
       p td1anzsco4 = tr.xpath("td[1]").inner_text.gsub(/\u00A0/,"").gsub(/\u200B/,"").strip
       td2nameen  = tr.xpath("td[2]").inner_text.gsub(/\u00A0/,"").gsub(/\u200B/,"").strip
       td3ceiling  = tr.xpath("td[3]").inner_text.gsub(/\u00A0/,"").gsub(/\u200B/,"").strip.to_i
-      td4result   = tr.xpath("td[4]").inner_text.gsub(/\u00A0/,"").gsub(/\u200B/,"").strip.to_i
+      # td4result   = tr.xpath("td[4]").inner_text.gsub(/\u00A0/,"").gsub(/\u200B/,"").strip.to_i
+      td4result = 0
 
-      db.execute("insert into ceilings (anzsco4, bbsid, nameen, namecn, ceiling, result, change) values (?,?,?,?,?,?,?)",
-        [td1anzsco4,anzbbs[td1anzsco4.to_i] , td2nameen, anzcn[td1anzsco4.to_i], td3ceiling, td4result, td4result])
+      db.execute("insert into ceilings (anzsco4, bbsid, nameen, namecn, ceiling) values (?,?,?,?,?)",
+        [td1anzsco4,anzbbs[td1anzsco4.to_i] , td2nameen, anzcn[td1anzsco4.to_i], td3ceiling])
+
+      db.execute("insert into ceilingslog (anzsco4) values (#{td1anzsco4})")
 
     end
 
@@ -58,7 +61,7 @@ def updatecsv()
 
   rows = db.execute("select anzsco4, bbsid, nameen, namecn, ceiling, result, change, ceiling - result as remain from ceilings order by remain")
 
-  CSV.open("#{DATADIR}#{F1718}.csv", "w") do |csv|
+  CSV.open("#{DATADIR}#{F1819}.csv", "w") do |csv|
     csv << %w(anzsco4 bbsid nameen namecn ceiling result change remain)
     rows.each do |row|
       csv << row
@@ -72,7 +75,7 @@ def postceiling()
 postctx =<<-POST
 ---
 layout: post
-title: 2017-2018 年度澳洲 SOL 配额完成情况 - #{CURRENTFN}
+title: 2018-2019 年度澳洲 SOL 配额完成情况 - #{CURRENTFN}
 date:  #{CURRENTFN} 13:00:00
 categories: gsm
 ---
@@ -100,9 +103,11 @@ categories: gsm
 {% endfor %}
 </table>
 
-更多说明请参考<a href="http://bbs.fcgvisa.com/t/2017-2018-sol-occupation-ceilings-for-the-2017-18-programme-year/24331" target="blank">飞出国论坛</a> 。
+更多说明请参考<a href="http://bbs.fcgvisa.com/t/2018-2019-sol-occupation-ceilings-for-the-2017-18-programme-year/24331" target="blank">飞出国论坛</a> 。
 
-需要获得相关移民及出国签证申请帮助可以联系飞出国微信（flyabroad）： <a href="http://flyabroad.me/contact" target="_blank">http://flyabroad.me</a>。
+[荷兰库拉索移民](http://www.flyabroad.hk/curacao)适合技术移民无望或技术移民遥遥无期的高知中产阶层人群。一套提供持续较高收益的国际房产（酒店公寓），一个说走就走的国际身份（无移民监），一个中产阶层与欧洲强国护照最接近的移民项目（荷兰护照）。
+
+需要获得相关移民及出国签证申请帮助可以联系飞出国： <a href="http://flyabroad.me/contact" target="_blank">http://flyabroad.me/contact/</a>。
 
 > 以上内容由`飞出国香港`（<a href="http://flyabroad.hk/" target="_blank">flyabroad.hk</a>）整理完成，转载请保留并注明出处。
 
@@ -187,7 +192,49 @@ def recreateceilingtable()
 
 end
 
+def recreateceilinglogtable()
+
+  db = SQLite3::Database.open "csol.db"
+
+    db.execute("Drop table if exists ceilingslog")
+    # anzsco4, bbsid, nameen, namecn, ceiling, result, change
+    rows = db.execute <<-SQL
+      create table ceilingslog (
+      Id INTEGER PRIMARY KEY AUTOINCREMENT,
+      anzsco4 TEXT DEFAULT NULL,
+      jullog INTERGER DEFAULT NULL,
+      auglog INTERGER DEFAULT NULL,
+      septlog INTERGER DEFAULT NULL,
+      octlog INTERGER DEFAULT NULL,
+      novlog INTERGER DEFAULT NULL,
+      declog INTERGER DEFAULT NULL,
+      janlog INTERGER DEFAULT NULL,
+      feblog INTERGER DEFAULT NULL,
+      marlog INTERGER DEFAULT NULL,
+      aprlog INTERGER DEFAULT NULL,
+      maylog INTERGER DEFAULT NULL,
+      junlog INTERGER DEFAULT NULL,
+      updated TEXT DEFAULT NULL
+      );
+    SQL
+    # 一月：January Jan.
+    # 二月：February Feb.
+    # 三月：March Mar.
+    # 四月：April Apr.
+    # 五月：May -
+    # 六月：June -
+    # 七月：July -
+    # 八月：August Aug.
+    # 九月：September Sept.
+    # 十月：October Oct.
+    # 十一月：November Nov.
+    # 十二月：December Dec.
+    db.close
+
+end
+
 # recreateceilingtable()
+# recreateceilinglogtable()
 # initdb()
 
 def maxeoi
@@ -201,7 +248,7 @@ def maxeoi
 
   puts "飞出国：本次邀请后澳大利亚技术移民 SOL 职业(189+489亲属)配额完成情况飞出国已经整理到网站，下表是飞出国整理的按照邀请人数由多到少的职业列表。"
 
-  tablearray.push "代码 | 职业名称 - 飞出国 | 17-18配额 | 本次邀请 | 剩余配额"
+  tablearray.push "代码 | 长表职业类别 - 飞出国 | 18-19配额 | 本次邀请 | 剩余配额"
   tablearray.push "---- | --------------- | -------- | -------- | -------"
 
   linkarray.push ""
@@ -228,8 +275,8 @@ def maxeoi
 
 end
 
-upateceilling()
-updatecsv()
-postceiling()
+# upateceilling()
+# updatecsv()
+# postceiling()
 
-maxeoi()
+# maxeoi()
