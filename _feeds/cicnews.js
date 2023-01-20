34a2030615b16ab93808a1c0dfc818cbb845fe31
fcgca <119646800+fcgca@users.noreply.github.com>
2023-01-19 20:40:29 -0800

    deleted:    _feeds/_cicnews/cicnews.sh 	modified:   _feeds/cicnews.js

--- _feeds/cicnews.js
+++ _feeds/cicnews.js
@@ -40,6 +40,7 @@ entries.forEach(async (element) => {
   shfilearray.push(`sed '2,22d;' "tmp_${mdfilename}" | sed 's/\\[\\([^][]*\\)\\]([^()]*)/\\1/g' | sed '/Tags:/,$d;/googletag/d;/cicnews/d;/Schedule a Free/d;/Get a Free/d;/Discover if You Are Eligible for Canadian Immigration/d;/Visit CanadaVisa.com/d;' | cat -s | tee ${mdfilename}`);
   shfilearray.push(`trans -b en:zh "file://${mdfilename}" | tee "${cnmdfilename}"`); 
   shfilearray.push(`sed -i 's/##* //g;s/^\\* //g;' ${mdfilename}`);
+  shfilearray.push(`sed -i 's/＃/#/g;' ${cnmdfilename}`);
   shfilearray.push(`paste "${cnmdfilename}" "${mdfilename}" | tee "${fcgmdfilename}"\n`);
   shfilearray.push(`cat "${jekyllfrontmatterfilename}" "${fcgmdfilename}" > "../../_posts/${fcgmdfilename}"\n`);
   shfilearray.push(`echo "\\nFCGvisa translated, © CIC News All Rights Reserved." >> "../../_posts/${fcgmdfilename}"\n`);
